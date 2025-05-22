3bf87e4764e6e1c83f38a4bce2a8b6bb50ccbbd9
bnealy <nealy.benjamin@gmail.com>
2025-05-22 16:02:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+// src/App.js
+import React from 'react';
+import { AuthProvider } from './contexts/AuthContext';
+import BowlingScorecard from './components/BowlingScorecard';
 
+function App() {
+  return (
+    <AuthProvider>
+      <div className="App">
+        <BowlingScorecard />
+      </div>
+    </AuthProvider>
+  );
+}
+
+export default App;
